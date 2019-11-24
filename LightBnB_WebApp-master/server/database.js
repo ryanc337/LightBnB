9d3800f2d08ee939825cfd0c8dff4be7d5190efc
Ryan <cordingley.ryan@gmail.com>
2019-11-24 08:34:08 +0000

    changed database.js file to include options of min, max price per night and min rating

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -9,10 +9,54 @@ const pool = new Pool({
 });
 
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`)
+    queryString += ` AND cost_per_night >= $${queryParams.length} * 100`
+  }
+  
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`)
+    queryString +=` AND cost_per_night <= $${queryParams.length} * 100`
+  }
+  
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `
+    GROUP BY properties.id
+    HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  } else {
+    queryString += `
+    GROUP BY properties.id`;
+  }
+  
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 };
 
@@ -21,7 +65,7 @@ const getUserWithEmail = function (email) {
   SELECT * FROM users
   WHERE email = $1`, [email])
   .then(res => res.rows[0] || null);
-}
+};
 
 const getUserWithId = function (id) {
   return pool.query(`SELECT * FROM users
